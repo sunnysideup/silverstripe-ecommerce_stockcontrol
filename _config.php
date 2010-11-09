ffba7295fad4d67b2a72d2778d1b399185363aed
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2010-11-09 06:53:38 +0000

    cleaned up _config.php
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/modules/ecommerce_stockcontrol/trunk@309 950b781c-c544-efab-2f99-90083ee69683

--- _config.php
+++ _config.php
@@ -1,10 +1,29 @@
 <?php
 
-MinMaxModifier::set_use_stock_quantities(true); //make use of the stock quantity tables to keep track of them
 
-DataObject::add_extension('Product', 'ProductStockDecorator');
-DataObject::add_extension('ProductVariation', 'ProductVariationStockDecorator');
 
-Order::set_modifiers(array(
-	'MinMaxModifier'
-));
+
+/**
+ * @author Nicolaas modules [at] sunnysideup.co.nz, + jeremy [at] burnbright.co.nz
+ *
+ **/
+
+
+//copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
+//===================---------------- START ecommerc_stockcontrol MODULE ----------------===================
+//MIN MAX MUST SET
+//Order::set_modifiers(array('MinMaxModifier'));
+// MIN MAX ONLY MAY SET
+//MinMaxModifier::set_default_min_quantity(1);
+//MinMaxModifier::set_default_max_quantity(99);
+//MinMaxModifier::set_min_field("MinQuantity");
+//MinMaxModifier::set_max_field("MaxQuantity");
+//MinMaxModifier::set_adjustment_message("Based on stock availability, quantities have been adjusted as follows: ");
+//MinMaxModifier::set_sorry_message("Sorry, your selected value not is available");
+//MinMaxModifier::set_show_adjustments_in_checkout_table("Sorry, your selected value not is available");
+
+//STOCK CONTROL MUST SET
+//MinMaxModifier::set_use_stock_quantities(true); //make use of the stock quantity tables to keep track of them
+//DataObject::add_extension('Product', 'ProductStockDecorator');
+//DataObject::add_extension('ProductVariation', 'ProductVariationStockDecorator');
+//===================---------------- END ecommerc_stockcontrol MODULE ----------------===================
