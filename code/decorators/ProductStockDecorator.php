55594549d0e41d191ed38a2841e3c2ba25e4390e
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2010-11-09 06:52:50 +0000

    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/modules/ecommerce_stockcontrol/trunk@308 950b781c-c544-efab-2f99-90083ee69683

--- code/decorators/ProductStockDecorator.php
+++ code/decorators/ProductStockDecorator.php
@@ -6,27 +6,27 @@
  */
 
 class ProductStockDecorator extends DataObjectDecorator{
-	
+
 	/*
 	 * Allow setting stock level in CMS
 	 */
 	function updateCMSFields(&$fields){
-		
+		$fields->addFieldToTab('Root.Content.Main',new NumericField('Stock','Stock'),'Content');
 		//FIXME: hack, because $this->Variations doesn't seem to work???
-		if(DataObject::get('ProductVariation',"\"ProductID\" = ".$this->owner->ID) == null){ 
+		if(DataObject::get('ProductVariation',"\"ProductID\" = ".$this->owner->ID) == null){
 			$fields->addFieldToTab('Root.Content.Main',new NumericField('Stock','Stock'),'Content');
 		}
-		
-		
+
+
 	}
-	
+
 	/*
 	 * Getter for stock level
 	 */
 	function getStock(){
 		return ProductStockCalculatedQuantity::get_quantity_by_product_id($this->owner->ID);
 	}
-	
+
 	/*
 	 * Setter for stock level
 	 */
@@ -34,7 +34,7 @@ class ProductStockDecorator extends DataObjectDecorator{
 		if(!$this->owner->ID){
 			$this->owner->TempStockValueToBeSaved = $value; //store for saving later (see onAfterWrite below)
 		}
-		
+
 		//only set stock level if it differs from previous
 		if($this->owner->ID && $value != $this->owner->getStock() && $member = Member::currentUser()){
 			$parent = ProductStockCalculatedQuantity::get_by_product_id($this->owner->ID);
@@ -45,7 +45,7 @@ class ProductStockDecorator extends DataObjectDecorator{
 			$obj->write();
 		}
 	}
-	
+
 	/*
 	 * Catch the case where stock level has been set on a new Product DataObject.
 	 */
@@ -55,7 +55,7 @@ class ProductStockDecorator extends DataObjectDecorator{
 			$this->owner->TempStockValueToBeSaved = null;
 		}
 	}
-	
+
 	/*
 	 * Only allow purchase if stock levels allow
 	 * TODO: customise this to a certian stock level, on, or off
@@ -66,5 +66,5 @@ class ProductStockDecorator extends DataObjectDecorator{
 		}
 		return null; //returning null ensures that can checks continue
 	}
-	
+
 }
