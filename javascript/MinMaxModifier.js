0a4edfde641bc2dbdd149da3e30c0c6530e52a73
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2011-08-17 16:04:33 +0000

    fixing little bugs in MinMaxModifier.js
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/modules/ecommerce_stockcontrol/trunk@1433 950b781c-c544-efab-2f99-90083ee69683

--- javascript/MinMaxModifier.js
+++ javascript/MinMaxModifier.js
@@ -8,28 +8,33 @@ var MinMaxModifier = {
 	add_item: function (fieldSelector, min, max, msg) {
 		jQuery(fieldSelector).blur(
 			function() {
-				var updated = false;
-				if(max > 0) {
-					if(jQuery(this).val() > max) {
-						jQuery(this).val(max);
-						updated = true;
-					}
-				}
-				if(min > 0) {
-					if(jQuery(this).val() < min) {
-						jQuery(this).val(min);
-						updated = true;
-					}
-				}
-				if(updated) {
-					if(MinMaxModifier.show_message) {
-						alert(msg);
-					}
-					jQuery(this).change();
-				}
+				MinMaxModifier.update_field(fieldSelector, min, max, msg);
 			}
 		);
 		jQuery(fieldSelector).blur();
-		MinMaxModifier.show_message = true;
+		//MinMaxModifier.show_message = true;
+	},
+
+	update_field: function(fieldSelector, min, max, msg) {
+		var updated = false;
+		if(max > 0) {
+			if(jQuery(fieldSelector).val() > max) {
+				jQuery(fieldSelector).val(max);
+				updated = true;
+			}
+		}
+		if(min > 0) {
+			if(jQuery(fieldSelector).val() < min) {
+				jQuery(fieldSelector).val(min);
+				updated = true;
+			}
+		}
+		if(updated) {
+			if(MinMaxModifier.show_message) {
+				alert(msg);
+			}
+			jQuery(fieldSelector).change();
+		}
 	}
+	
 }
