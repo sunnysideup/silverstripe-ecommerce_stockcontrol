d395b97d0e72d8639cabbb23ff74c8bffba94381
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2011-09-25 05:59:06 +0000

    adding new stock control files
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/modules/ecommerce_stockcontrol/trunk@1508 950b781c-c544-efab-2f99-90083ee69683

---
+++ code/StockControlController.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * @author Nicolaas [at] sunnysideup.co.nz
+ * @package: ecommerce
+ * @sub-package: ecommerce_stockcontrol
+ * @description:
+ *  This is the central management page for organising stock control
+ *  You will need to "turn on" the MinMaxModifier and add MinMaxModifier::set_use_stock_quantities(true)
+ *  to get this page working.
+ *
+ *
+ **/
+
+
+
+
+class StockControlController extends ContentController {
+
+
+	protected static  $url_segment ="update-stock";
+	public static function get_url_segment(){return self::$url_segment;}
+	public static function set_url_segment($s){self::$url_segment = $s;}
+	//TODO: move all this to CMS
+
+
+	function init() {
+		// Only administrators can run this method
+		if(!Permission::check("ADMIN")) {
+			Security::permissionFailure($this, _t('Security.PERMFAILURE',' This page is secured and you need administrator rights to access it. Enter your credentials below and we will send you right along.'));
+		}
+		parent::init();
+		Requirements::themedCSS("StockControlPage");
+		Requirements::javascript(THIRDPARTY_DIR."/jquery/jquery.js");
+		Requirements::javascript("ecommerce_stockcontrol/javascript/StockControlPage.js");
+		$url = Director::absoluteURL($this->Link()."update/");
+		Requirements::customScript("StockControlPage.set_url('".$url."');", "StockControlPage.set_url");
+	}
+
+	function Link(){
+		return "/".self::get_url_segment()."/";
+	}
+
+	function StockProductObjects() {
+		$buyableStockCalculatedQuantities = DataObject::get("BuyableStockCalculatedQuantity", "UnlimitedStock = 0", "", "", "10");
+		if($buyableStockCalculatedQuantities) {
+			foreach($buyableStockCalculatedQuantities as $buyableStockCalculatedQuantity) {
+				if($buyableStockCalculatedQuantity->Buyable()->UnlimitedStock) {
+					$buyableStockCalculatedQuantities->remove($buyableStockCalculatedQuantity);
+				}
+				else {
+					$buyableStockCalculatedQuantity->calculatedBaseQuantity();
+				}
+			}
+			return $buyableStockCalculatedQuantities;
+		}
+	}
+
+	function update($request = null) {
+		$id = intval($request->param("ID"));
+		$newValue = intval($request->param("OtherID"));
+		if($newValue || $newValue === 0) {
+			if($obj = DataObject::get_by_id("BuyableStockCalculatedQuantity", $id)) {
+				if($buyable = $obj->getBuyable()) {
+					$buyable->setActualQuantity($newValue);
+					$msg = "<em>".$obj->Name . "</em> quantity updated to <strong>".$newValue."</strong>";
+					return $this->customise(array("Message" => $msg))->renderWith("UpdateStockQuantity");
+				}
+				else {
+					user_error("Could not create Calculation object", E_USER_NOTICE);
+				}
+			}
+			else {
+				user_error("could not find record: $id ", E_USER_NOTICE);
+			}
+		}
+		else {
+			user_error("new quantity specified is unknown", E_USER_NOTICE);
+		}
+	}
+
+ 	function history($request = null) {
+		$id = intval($request->param("ID"));
+		$buyableStockCalculatedQuantity = DataObject::get_by_id("BuyableStockCalculatedQuantity", $id);
+		if($buyableStockCalculatedQuantity) {
+			$buyableStockCalculatedQuantity->ManualUpdates = DataObject::get("BuyableStockManualUpdate", "\"ParentID\" = ".$buyableStockCalculatedQuantity->ID);
+			$buyableStockCalculatedQuantity->OrderEntries = DataObject::get("BuyableStockOrderEntry", "\"ParentID\" = ".$buyableStockCalculatedQuantity->ID);
+			/*
+			$graphArray = array();
+			if($buyableStockCalculatedQuantity->ManualUpdates) {
+				foreach($buyableStockCalculatedQuantity->ManualUpdates as $obj) {
+				}
+			}
+			if($buyableStockCalculatedQuantity->OrderEntries) {
+				foreach($buyableStockCalculatedQuantity->OrderEntries as $obj) {
+				}
+			}
+			*/
+			return $this->customise($buyableStockCalculatedQuantity)->renderWith("AjaxStockControlPageHistory");
+		}
+		else {
+			return " could not find historical data";
+		}
+ 	}
+
+
+}
---
+++ templates/StockControlController.ss
@@ -0,0 +1,38 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<title>Update Your Stock</title>
+</head>
+<body>
+
+<div class="StockObjectsFeedback"><p>Please adjust available quantities below.</p></div>
+<ul id="StockProductObjects">
+<% control StockProductObjects %>
+	<li>
+		<span class="currentNumber item$ID">$BaseQuantity =</span>
+		<input type="text" value="" name="update/$ID" class="updateField productUpdateField" />
+		<label class="history"><a href="$HistoryLink" rel="history{$BuyableClassName}{$BuyableID}">$Name</a></label>
+		<div class="loadHistoryHere" id="history{$BuyableClassName}{$BuyableID}"></div>
+	</li>
+<% end_control %>
+<% if StockProductObjects.MoreThanOnePage %>
+	<% if StockProductObjects.NotFirstPage %>
+		<a class="prev" href="/$StockProductObjects.PrevLink">Prev</a>
+	<% end_if %>
+ <% control StockProductObjects.PaginationSummary(10) %>
+		<% if CurrentBool %>
+			$PageNum
+		<% else %>
+			<a href="/$Link">$PageNum</a>
+		<% end_if %>
+	<% end_control %>
+	<% if StockProductObjects.NotLastPage %>
+		<a class="next" href="/$StockProductObjects.NextLink">Next</a>
+	<% end_if %>
+<% end_if %>
+</ul>
+<div class="StockObjectsFeedback"><p>Please adjust available quantities above.</p></div>
+
+</body>
+</html>
